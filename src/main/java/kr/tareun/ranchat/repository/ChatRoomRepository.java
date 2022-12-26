f9955b5d086cc79045c0221bde4ea6f4773fcd15
Tareun3406 <tareun3406@gmail.com>
2022-12-26 17:32:44 +0900

    refactor ChatRoomRepository with implement interface

--- src/main/java/kr/tareun/ranchat/repository/ChatRoomRepository.java
+++ src/main/java/kr/tareun/ranchat/repository/ChatRoomRepository.java
@@ -2,57 +2,9 @@ package kr.tareun.ranchat.repository;
 
 import kr.tareun.ranchat.model.vo.ChatMessageVO;
 import kr.tareun.ranchat.model.vo.ChatRoomVO;
-import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
-
-@Repository
-public class ChatRoomRepository {
-    Queue<ChatRoomVO> roomQueue = new LinkedList<>();  // 대기열에 등록된 방
-    Map<String,ChatRoomVO> rooms= new HashMap<>(); // 생성된 방목록
-
-
-    // 랜덤매칭 시작
-    public ChatRoomVO matchRoom(){
-        ChatRoomVO room;
-        if (roomQueue.isEmpty()){               // 대기중인 방이 없을 경우
-            room = createRoom();                // 새로운 방 생성
-            roomQueue.add(room);   // 생성된 방 대기열에 등록
-        }else {
-            room = roomQueue.remove(); // 대기열에 등록된 방 가져오기
-            if (!rooms.containsKey(room.getRoomId())){ // 대기열에 있었지만 방목록이 사라진 경우.
-                rooms.put(room.getRoomId(), room);
-                roomQueue.add(room);
-            }
-        }
-        return room;
-    }
-
-    // 채팅방 입장
-    public void joinRoom(ChatMessageVO message){
-        String joiner = message.getWriter();
-        rooms.get(message.getRoomId()).joinMember(joiner);
-    }
-
-    // 채팅방 퇴장
-    public void outRoom(ChatMessageVO message){
-        String outer = message.getWriter();
-        String roomId = message.getRoomId();
-        int remaining = rooms.get(roomId).outMember(outer);
-        if(remaining == 0){
-            rooms.remove(roomId);   // 남은 인원이 없을경우 채팅방 삭제
-        }
-    }
-
-    // 채팅방 생성
-    private ChatRoomVO createRoom(){
-        ChatRoomVO room = new ChatRoomVO();
-        rooms.put(room.getRoomId(), room);
-        return room;
-    }
-
-
+public interface ChatRoomRepository {
+    public ChatRoomVO matchRoom();
+    public void joinRoom(ChatMessageVO message);
+    public void outRoom(ChatMessageVO message);
 }
