a8ed41061e6509a8671cfb82e8b4b737ec503450
Tareun3406 <tareun3406@gmail.com>
2023-01-28 00:12:17 +0900

    fix message ui delete debug code

--- frontend/src/component/util/GetUserId.ts
+++ frontend/src/component/util/GetUserId.ts
@@ -3,7 +3,6 @@ import {useEffect, useState} from "react";
 const GetUserId: any = ()=>{
     const [userId,setUserId] = useState<string>();
     useEffect(()=>{
-        console.log("로그인 정보 가져오기");
         fetch("/getLoginMember")
             .then((response)=>{
                 return response.text();
